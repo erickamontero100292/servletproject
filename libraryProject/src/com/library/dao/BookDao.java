06dccd9ea26ab81816f7ca919bfd4ea6850cd777
erickamontero <monteroh.ericka@gmail.com>
2019-08-07 19:30:52 -0500

    development of connection for database library

--- libraryProject/src/com/library/dao/BookDao.java
+++ libraryProject/src/com/library/dao/BookDao.java
@@ -1,37 +1,42 @@
 package com.library.dao;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.library.model.Book;
+
 public class BookDao {
     private DbConnection conn;
 
     public BookDao() {
-      
+
     }
+
     public BookDao(DbConnection conn) {
         this.conn = conn;
     }
-	
-    public List<Book> getUltimos() {
- 
-        try {
 
+    public List<Book> getUltimos() {
+
+        try {
+            String sql = "select * from book order by id desc limit 3";
             List<Book> list = new LinkedList<>();
-          	 Book book = new Book("Code clean");
-             Book book2 = new Book("Java 8");
-             Book book3 = new Book("SQL");
-            list.add(book);
-            list.add(book2);
-            list.add(book3);
-                System.out.println(list);
-            
+            PreparedStatement preparedStatement = conn.getConnection().prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            Book book;
+            while (rs.next()) {
+                book = new Book(rs.getInt("id"));
+                book.setName(rs.getString("name"));
+                book.setAuthor(rs.getString("author"));
+                book.setDatePublished(rs.getDate("date_published"));
+                list.add(book);
+            }
+
             return list;
 
-        } catch (Exception e) {            
+        } catch (Exception e) {
             System.out.println("Error BookDao.getUltimos: " + e.getMessage());
             return null;
         }
