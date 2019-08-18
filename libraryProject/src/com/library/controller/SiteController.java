fb9376ae98c185a4886abe4f0aad07cbf13f88e7
erickamontero <monteroh.ericka@gmail.com>
2019-08-18 18:33:02 -0500

    load file in the module of request

--- libraryProject/src/com/library/controller/SiteController.java
+++ libraryProject/src/com/library/controller/SiteController.java
@@ -19,7 +19,6 @@ public class SiteController extends HttpServlet {
 
 	public SiteController() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -36,7 +35,6 @@ public class SiteController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
